c1f28b3929fb67146ab1dde61752978c9f0f4e6f
Chinoms <chinoms925@gmail.com>
2021-07-07 12:53:18 +0100

    Update articleheader.php

--- inc/articleheader.php
+++ inc/articleheader.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title><?php echo $postData['post_title']; ?></title>
-    <meta name="description" content="<?php echo $postData['post_description']; ?>">
+    <meta name="description" content="<?php echo $postData['post_seo_desc']; ?>">
     <meta name="keywords" content="<?php echo $postData['post_keywords']; ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
